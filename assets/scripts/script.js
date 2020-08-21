f7b7dbdcb6241a1b8383f45dab86b26f78274ab7
Aravind Suresh <31103436+AravindSureshThakidayil@users.noreply.github.com>
2020-08-21 18:56:52 +0530

    Rewritten for Firestore RD and AngularJS-related code removed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,24 @@
-var wqm = angular.module("wqm", []);
+var g = []
 
-var r = []
-data.collection("salinity-stations").get().then((k) => {
-  k.forEach((d) => {
-    r.push(d.data());
+function getSnapshot(k) {
+  var ref = data.ref(k + '/');
+  ref.on("value", function (s) {
+    g.push(s.val());
   });
-});
+}
+
+for (let i = 1; i <= 3; i++) {
+  getSnapshot(i);
+}
+
+function show() {
+  $("#rightpane").show();
+  var k = g[parseInt($("[name='station_number']").val())-1];
+  $("#name").text(k.Name);
+  $("#salinity").text(k.Salinity);
+  $("#date").text(k.LastUpdated);
+}
 
 $(function () {
   $("#rightpane").hide();
-});
-
-wqm.controller("main", ($scope) => {
-  $scope.stations = r;
-  $scope.show = function () {
-    $("#rightpane").toggle();
-  };
 });
